5a75853a4db9e2f62ba0f2ef45685e5e0c6bf7b4
frkodes <frkalderon@gmail.com>
2019-05-16 19:15:14 -0500

    Staring single product the layout fix

--- single-producto.php
+++ single-producto.php
@@ -26,11 +26,76 @@ get_header();
 
 						<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 							<div class="container">
+
+								<div class="row">
+
+									<header class="entry-header product-detail">
+										<?php get_template_part( 'template-parts/header/entry', 'header' ); ?>
+									</header>
+								</div>
+
+
 								<div class="row">
 									<div class="col-xs-12 col-md-6 detail-image-container">
 										<figure class="zoom" onMouseMove="zoom(event)" style="background-image: url(<?php the_field('imagen_detalle'); ?>);">
 											<img src="<?php the_field('imagen_detalle'); ?>" alt="<?php the_title() ?>">
 										</figure>
+
+										<div class="icons-container">
+											<?php
+											$features = get_field('features');
+											$feature_container = "";
+											foreach( $features as $feature ) { ?>
+												<div class="feature-container">
+													<span class="feature-icon icon-<?php echo strtolower($feature); ?>"></span>
+													<?php switch ($feature) {
+														case 'anti-acaros':
+															$feature_description = 'Anti ácaros';
+															break;
+														case 'bloque-60k':
+															$feature_description = 'Bloque 60K';
+															break;
+														case 'bonell-ortopedico':
+															$feature_description = 'Núcleo Bonell ortopédico';
+															break;
+														case 'bordes-reforzados':
+															$feature_description = 'Border reforzados';
+															break;
+														case 'cero-movimiento':
+															$feature_description = 'Cero movimiento';
+															break;
+														case 'colchon-personalizable':
+															$feature_description = 'sistema personalizable';
+															break;
+														case 'extra-soporte-lumbar':
+															$feature_description = 'Extra soporte lumbar';
+															break;
+														case 'memory-foam':
+															$feature_description = 'Memory foam';
+															break;
+														case 'nano-tratamiento':
+															$feature_description = 'Nano Tratamiento repelente de manchas y líquidos';
+															break;
+														case 'pocket-spring':
+															$feature_description = 'Pocket Spring';
+															break;
+														case 'retardante-fuego':
+															$feature_description = 'Tratamiento ignifuego';
+															break;
+														case 'sistema-encapsulado':
+															$feature_description = 'Sistema encapsulado';
+															break;
+														case 'sistema-sin-resortes':
+															$feature_description = 'Sistema sin resortes';
+															break;
+														default:
+															$feature_description = '';
+															break;
+													} ?>
+													<p class="feature-description"><?php echo $feature_description; ?></p>
+												</div>
+											<?php } ?>
+										</div>
 									</div>
 									<div class="col-xs-12 col-md-6">
 										<header class="entry-header product-detail">
--- template-parts/header/entry-header.php
+++ template-parts/header/entry-header.php
@@ -7,29 +7,4 @@
  * @since 1.0.0
  */
 
-the_title( '<h1 class="entry-title mb-40 mt-20">', '</h1>' ); ?>
-
-<?php if ( ! is_page() ) : ?>
-<div class="entry-meta">
-	<span class="comment-count">
-	</span>
-	<?php
-	// Edit post link.
-		edit_post_link(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers. */
-					__( 'Edit <span class="screen-reader-text">%s</span>' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				get_the_title()
-			),
-			'<span class="edit-link">Editar</span>'
-		);
-	?>
-</div><!-- .meta-info -->
-<?php endif; ?>
+the_title( '<h1 class="entry-title">', '</h1>' ); ?>
